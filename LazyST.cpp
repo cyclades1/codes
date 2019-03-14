98d2c42c62191c6a6d80a2c589239cba9102c154
Abhishek Kumar Dubey <42466362+cyclades1@users.noreply.github.com>
2019-03-14 15:52:59 +0530

    Update Segment Tree(LP)

--- LazyST.cpp
+++ LazyST.cpp
@@ -1,14 +1,3 @@
-/*
-// Sample code to perform I/O:
-
-
-cin >> name;                            // Reading input from STDIN
-cout << "Hi, " << name << ".\n";        // Writing output to STDOUT
-
-// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
-*/
-
-// Write your code here
 #include<bits/stdc++.h>
 using namespace std;
 
